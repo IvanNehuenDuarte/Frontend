1ef034005c32fcbfaf4191742e88b62109013b8f
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-04-14 13:23:12 -0300

    Confetti ganador

--- projects/02-ta-te-ti/src/App.jsx
+++ projects/02-ta-te-ti/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import confetti from "canvas-confetti";
 
 const TURNS = {
   X: "X",
@@ -74,6 +75,7 @@ function App() {
     // Revisar si hay ganador
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
+      confetti()
       setWinner(newWinner)
     } else if (checkEndGame(newBoard)) {
       setWinner(false) // Empate
