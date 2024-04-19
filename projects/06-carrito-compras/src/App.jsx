ec9219aff173fe935d2269719ca7c8bc0d5f6fee
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-04-19 18:38:56 -0300

    Component products, show products and filter products

---
+++ projects/06-carrito-compras/src/App.jsx
@@ -0,0 +1,30 @@
+import { products as initialProducts } from './mocks/products.json'
+import { Products } from './components/Products.jsx'
+import { useState } from 'react'
+
+function App () {
+    const [products] = useState(initialProducts)
+    const [filters, setFilter] = useState({
+        category: 'all',
+        minPrice: 0
+    })
+
+    const filterProducts = (products) => {
+        return products.filter(product => {
+            return (
+                product.price >= filters.minPrice && (
+                    filters.category === 'all' ||
+                    product.category === filters.category
+                )
+            )
+        })
+    }
+
+    const filteredProducts = filterProducts(products)
+
+    return (
+        <Products products={filteredProducts}/>
+    )
+}
+
+export default App
